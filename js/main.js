c27311f39531c59fad5ad884fa6565639b221cd6
Johnathan Choi <john.choi.447@gmail.com>
2017-09-04 19:06:25 -0700

    Navbar is now tabs. No longer using materialize.css

--- js/main.js
+++ js/main.js
@@ -3,3 +3,24 @@ Johnathan Choi 2017
 main.js
 JS file that supports index.html. Gradual phase-in from materialize.js
 */
+
+function openTab(evt, tabName) {
+    // Declare all variables
+    var i, tabcontent, tablinks;
+
+    // Get all elements with class="tabcontent" and hide them
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    // Get all elements with class="tablinks" and remove the class "active"
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+
+    // Show the current tab, and add an "active" class to the button that opened the tab
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
